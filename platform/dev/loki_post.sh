63cd2a1ec571f2967c864b9e7838ee8d6c798b89
Gary Miller <gary@helixta.com.au>
2021-12-08 15:09:25 +1100

    grafana/loki:2.4.1 & grafana/grafana:8.3.1

--- platform/dev/loki_post.sh
+++ platform/dev/loki_post.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -v -H "Content-Type: application/json" -XPOST -s "http://localhost:3100/loki/api/v1/push" --data-raw \
-  '{"streams": [{ "stream": { "env": "dev" }, "values": [ [ "'`date +%s`000000000'", "{3: \"fizz\", 5: \"buzz\", 15: \"fizzbuzz\"}" ] ] }]}'
+  '{"streams": [{ "stream": { "env": "dev" }, "values": [ [ "'`date +%s`000000000'", "{\"3\": \"fizz\", \"5\": \"buzz\", \"15\": \"fizzbuzz\"}" ] ] }]}'
